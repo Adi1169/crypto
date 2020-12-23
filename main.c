afbddb087efe22ed47e3bb81b06e871546cf8a3a
Adi1169 <adityahfjc17@gmail.com>
2020-12-23 09:18:58 +0530

    third

--- main.c
+++ main.c
@@ -1,49 +1,63 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stddef.h>
-#include "crypto/bip39.h"
-#include "crypto/hasher.h"
-#include "crypto/secp256k1.h"
-#include "crypto/ecdsa.h"
 
-
+#include "base58.h"
+#include "bip32.h"
+#include "bip39.h"
+#include "curves.h"
+#include "ecdsa.h"
+#include "secp256k1.h"
+#include "sha2.h"
+#include "ripemd160.h"
 int main(void) 
 {
-  const char *mnemonic = "garden reject beauty inch scissors rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";//mnemonic_generate(256);
-  
-  
-  char *passphrase = "";
-  uint8_t bip39seed[512 / 8],privatekey[256/8],checksum[8],publickey[33],pubhash[20];
-	printf("%s\n", mnemonic);
-	printf("\n");
-
-   	mnemonic_to_seed(mnemonic, passphrase, bip39seed, 0);
+  	const char *mnemonic = "garden reject beauty inch scissors rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";//mnemonic_generate(256);
+	char *passphrase = "";
+	//const char *curve = &secp256k1;
+	HDNode master;
+	uint8_t bip39seed[512 / 8],publickey[32];
+	uint8_t hash[SHA256_DIGEST_LENGTH],address[25],versionkey[21],dhash[SHA256_DIGEST_LENGTH],ripe[RIPEMD160_DIGEST_LENGTH],addresscomp[34];
+	
+	mnemonic_to_seed(mnemonic, passphrase, bip39seed, 0);
+	
+	int x=hdnode_from_seed(bip39seed,64,SECP256K1_NAME,&master);
+	hdnode_fill_public_key(&master);
 
 	
-	printf("privatekey in hex\n\n");
-	for(int i=0;i<32;++i){
-		privatekey[i]=bip39seed[i];
-		printf("%02X",bip39seed[i]);
-	}
-	printf("\n");
-	printf("\n");
-	const ecdsa_curve *curve = &secp256k1;
-	ecdsa_get_public_key33(curve,privatekey, publickey);
-	printf("publickey in hex\n\n");
-	for(int i=0;i<33;++i){
-		printf("%02X",publickey[i]);
-	}
-	printf("\n");
-	printf("\n");
-	ecdsa_get_pubkeyhash(publickey,HASHER_SHA2_RIPEMD,pubhash);
+	sha256_Raw(&master.public_key,sizeof(master.public_key),hash);
+
 	
-	printf("pubhash in hex\n\n");
-	for(int i=0;i<20;++i){
-		printf("%02X",pubhash[i]);
-	}
+	ripemd160(hash, sizeof(hash), &ripe);
 	
+	versionkey[0]=(char)0;
+	memcpy(&versionkey[1],ripe,sizeof(ripe));
+	
+	sha256_Raw(&versionkey,sizeof(versionkey),hash);
+	
+	sha256_Raw(&hash,sizeof(hash),dhash);
 
+	
+	for(int i=0;i<21;++i)address[i]=versionkey[i];
+	for(int i=21,j=0;i<25;++i,++j)address[i]=dhash[j];
 
+	
+	uint16_t len =(char)34;
+	b58enc(addresscomp,&len,address, 25);
 
-  	return 0;
+	printf("chain code-->");
+	for(int i=0;i<32;++i)printf("%02X",master.chain_code[i]);
+	printf("\n");
+	
+	printf("public key-->");
+	for(int i=0;i<33;++i){printf("%02X",master.public_key[i]);publickey[i]=master.public_key[i];}
+	printf("\n");
+
+	printf("address-->");
+	for(int i=0;i<34;++i)printf("%c",addresscomp[i]);
+	printf("\n");
+	
+	
+	
+	
 }
